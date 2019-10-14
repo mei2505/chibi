75f31fe51e5718cd94fcac31527628639d6e4167
TAMURA Miyu <21716061@jwu.ac.jp>
2019-10-14 16:49:43 +0900

    eval7

--- exp.py
+++ exp.py
@@ -1,4 +1,16 @@
-class Val(object):
+class Expr(object):
+    def eval(self):
+        pass
+
+class Binary(Expr):
+    def __init__(self,left,right):
+        self.left=Expr(left)
+        self.right=Expr(right)
+    def __repr__(self):
+        classname=self.__class__.__name__
+        return f'{classname}({self.left},{self.right})'
+
+class Val(Expr):
     __slots__=["value"]
     def __init__(self,value=0):
         self.value=value
@@ -13,9 +25,13 @@ v=Val(1)
 assert v.eval()==1
 print(v)
 
-class Add(object):
+class Add(Expr):
     __slots__=["left","right"]
     def __init__(self,a,b):
+        if not isinstance(a,Expr):
+            a=Val(a)
+        if not isinstance(b,Expr):
+            b=Val(b)
         self.left=a
         self.right=b
     def eval(self):
@@ -29,7 +45,7 @@ assert ee.eval()==6
 print(e.eval())
 print(ee.eval())
 
-class Sub(object):
+class Sub(Expr):
     __slots__=["left","right"]
     def __init__(self,a,b):
         self.left=a
@@ -41,7 +57,7 @@ e=Sub(Val(2),Val(1))
 assert e.eval()==1
 print(e.eval())
 
-class Mul(object):
+class Mul(Expr):
     __slots__=["left","right"]
     def __init__(self,a,b):
         self.left=a
@@ -53,7 +69,7 @@ e=Mul(Val(2),Val(2))
 assert e.eval()==4
 print(e.eval())
 
-class Div(object):
+class Div(Expr):
     __slots__=["left","right"]
     def __init__(self,a,b):
         self.left=a
@@ -64,3 +80,6 @@ class Div(object):
 e=Div(Val(7),Val(2))
 assert e.eval()==3
 print(e.eval())
+
+assert isinstance(Val(1))
+assert isinstance(Div(Val(7),Val(2)),Expr)
