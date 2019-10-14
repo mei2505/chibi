107b18e6dd454fd61aac2c14afc8711e1e7bc828
TAMURA Miyu <21716061@jwu.ac.jp>
2019-10-14 16:01:34 +0900

    eval2

--- exp.py
+++ exp.py
@@ -5,10 +5,26 @@ class Val(object):
 
     def eval(self):
         return self.value
-    
+
     def __repr__(self):
         return f"Val({self.value})"
 
 v=Val(1)
 assert v.eval()==1
 print(v)
+
+class Add(object):
+    __slots__=["left","right"]
+    def __init__(self,a,b):
+        self.left=a
+        self.right=b
+    def eval(self):
+        return self.left.eval()+self.right.eval()
+
+e=Add(Val(1),Val(2))
+assert e.eval()==3
+e=Add(Val(1),Add(Val(2),Val(3)))
+assert e.eval()==6
+
+print(e)
+print(e)
