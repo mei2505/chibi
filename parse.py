f0dcf391388a851816acad19fb59d330bcbcdcac
TAMURA Miyu <21716061@jwu.ac.jp>
2019-10-28 16:44:43 +0900

    output Add,Sub,Mul,Div

--- parse.py
+++ parse.py
@@ -40,12 +40,12 @@ def parse(s:str):
         s1=s[0:pos]
         s2=s[pos+1:]
         return Mul(parse(s1),parse(s2))
-    if s.find("/")>0:
-        pos=s.find("/")
+    if s.rfind("/")>0:
+        pos=s.rfind("/")
         s1=s[0:pos]
         s2=s[pos+1:]
         return Div(parse(s1),parse(s2))
     return Val(int(s))
 
-e=parse("1-2-3")
+e=parse("8/4/2")
 print(e,e.eval())
