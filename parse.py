7a3de0d3867df4e449f34dbd145198c98eca4e63
TAMURA Miyu <21716061@jwu.ac.jp>
2019-10-28 16:43:07 +0900

    output Add,Sub,Mul,Div

--- parse.py
+++ parse.py
@@ -30,8 +30,8 @@ def parse(s:str):
         s1=s[0:pos]
         s2=s[pos+1:]
         return Add(parse(s1),parse(s2))
-    if s.find("-")>0:
-        pos=s.find("-")
+    if s.rfind("-")>0:
+        pos=s.rfind("-")
         s1=s[0:pos]
         s2=s[pos+1:]
         return Sub(parse(s1),parse(s2))
@@ -47,5 +47,5 @@ def parse(s:str):
         return Div(parse(s1),parse(s2))
     return Val(int(s))
 
-e=parse("1+2*3")
+e=parse("1-2-3")
 print(e,e.eval())
